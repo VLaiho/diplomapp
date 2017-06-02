ce672aac06d24f7585992247a6fa7b9a6d557c1c
Valit Laiho <heelmistico@gmail.com>
2017-06-02 15:26:25 +0300

    Full changes in students table

--- db/migrate/20170601221226_change_name_in_student_to_text.rb
+++ db/migrate/20170601221226_change_name_in_student_to_text.rb
@@ -1,5 +1,9 @@
 class ChangeNameInStudentToText < ActiveRecord::Migration[5.0]
   def change
   	change_column :students, :name, :text 
+  	change_column :students, :birth_date, :text
+  	change_column :students, :school_name, :text
+  	change_column :students, :address, :text
+  	change_column :students, :passport_data, :text
   end
 end
